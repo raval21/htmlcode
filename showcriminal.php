1db5491e70dfaac2599e88a157d1108dcfa9203e
raval21 <ravalraj212@gmail.com>
2023-08-31 22:57:49 -0700

    new

--- showcriminal.php
+++ showcriminal.php
@@ -58,13 +58,7 @@ $conn->close();
 <body>
   <header>
     <div class="container1">
-        <form class="form-inline my-2 my-lg-0 ">
-          <input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0 " type="submit">Search</button>
-        </form>
-       </div>
-    <!-- place navbar here -->
-  </header>
+         </header>
   <main>
 <div class="container p-5 ">
   <div class="table-responsive">
